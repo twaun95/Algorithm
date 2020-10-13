604a9b5afbfdfce1249fab0280cb81d3aa297a7b
twaun95 <kandy678@naver.com>
2020-10-13 16:23:25 +0000

    Binary Search 예제2

--- Binary Search/2.py
+++ Binary Search/2.py
@@ -1,25 +1,40 @@
-#반복문이용
-
-def binary_search(array, target, start, end):
-  while start <= end:
-    mid = (start + end) //2
-    if array[mid] == target:
-      return mid
-    elif array[mid] > target:
-      end = mid -1
-    else:
-      start = mid + 1
-  return None
-
-n=int(input())
+#떡볶이 떡 만들기
+"""
+오늘 태완이는 여행 가신 부모님을 대신해서 떡집 일을 하기로 했다. 오늘은 떡볶이 떡을 만드는 날이다.
+태완이네 떡볶이 떡은 재밌게도 떡볶이 떡의 길이가 일정하지 않다. 대신에 한 봉지 안에 들어가는 떡의 총 길이는 절단기로 잘라서 맞춰준다.
+절단기에 높이(H)를 지정하면 줄지어진 떡을 한 번에 절단한다. 높이가 H보다 긴 떡은 H 위의 부분이 잘릴 것이고, 낮은 떡은 잘리지 않는다. 
+예를 들어 높이가 19, 14, 10, 17cm인 떡이 나란히 있고 절단기 높이를 15cm로 지정하면 자른 뒤 떡의 높이는 15, 14, 10, 15cm가 될 것이다. 잘린 떡의 길이는 차례대로 4, 0, 0, 2cm이다. 손님은 6cm만큼의 길이를 가져간다.
+손님이 왔을 때 요청한 총 길이가 M일 때 적어도 M만큼의 떡을 얻기 위해 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램을 작성하시오.
+"""
+"""
+*입력조건
+- 첫째 줄에 떡의 개수 N과 요청한 떡의 길이 M이 주어진다. (1<N<1,000,000 / 1<M<2,000,000,000)
+- 둘쨰 줄에는 떡의 개별 높이가 주어진다. 떡 높이의 총합은 항상 M 이상이므로, 손님은 필요한 양만큼   떡을 사갈 수 있다. 높이는 10억보다 작거나 같은 양의 정수 또는 0이다.
+*출력조건
+- 적어도 M만큼의 떡을 집에 가져가기 위해 절단기에 설정할 수 있는 높이의 최댓값을 출력한다.
+"""
+"""
+입력예시          출력예시
+4 6              15
+19 15 10 17
+"""
+N,M = list(map(int, input().split(' ')))
 array = list(map(int, input().split()))
-array.sort()
-m = int(input())
-x = list(map(int, input().split()))
 
-for i in x:
-  result = binary_search(array, i, 0, n-1)
-  if result != None:
-    print('yes', end=' ')
+start = 0
+end = max(array)
+
+result = 0
+while(start <= end):
+  total = 0
+  mid = (start + end) // 2
+  for x in array:
+    if x > mid:
+      total += x - mid
+  if total < M:
+    end = mid - 1
   else:
-    print('no', end=' ')
+    result = mid
+    start = mid + 1
+
+print(result)
