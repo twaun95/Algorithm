5cee7659cda817310803a252789407b156fd2f9a
twaun95 <>
2021-04-21 09:52:42 +0000

    백준 18406

---
+++ 구현/럭키스트레이트.py
@@ -0,0 +1,20 @@
+#https://www.acmicpc.net/problem/18406
+
+n = input()
+
+length = len(n)
+sum1 = 0
+sum2 = 0
+
+for i in range(length//2):
+    sum1 += int(n[i])
+
+for i in range(length//2,length):
+    sum2 += int(n[i])
+
+if sum1 == sum2:
+    print("LUCKY")
+else:
+    print("READY")
+
+
