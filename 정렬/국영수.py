45b6c19910efe8dd43887bbd72e0b18470ce6dae
twaun95 <43190509+twaun95@users.noreply.github.com>
2021-05-03 22:57:38 +0900

    Update 국영수.py

--- 정렬/국영수.py
+++ 정렬/국영수.py
@@ -7,6 +7,7 @@ student = []
 for i in range(n):
     student.append(input().split())
 
+#-는 역순으로
 sorted_student = sorted(student, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for i in range(n):
