932cc361e759ee25aa8b1e3e8a7012f200626012
twaun95 <43190509+twaun95@users.noreply.github.com>
2021-05-05 22:57:35 +0900

    Update 카드정렬하기.py

--- 정렬/카드정렬하기.py
+++ 정렬/카드정렬하기.py
@@ -7,11 +7,11 @@ card=[]
 for _ in range(n):
     heapq.heappush(card,int(input()))
 
-result = 0
+result = 0 #결과(출력값)
 while len(card)>1:
-    A = heapq.heappop(card)
-    B = heapq.heappop(card)
-    heapq.heappush(card, A+B)
-    result += A+B
+    A = heapq.heappop(card) #제일 작은 카드 HEAP에서 빼기
+    B = heapq.heappop(card) #그 다음 작은 카드 HEAP에서 빼기
+    heapq.heappush(card, A+B) # A,B합치고 다시 HEAP에 넣기
+    result += A+B #결과 - 두 값을 계속 더하는 값
 
 print(result)
