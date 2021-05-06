0edbe4971fffeded4516441b722a12437beb8768
twaun95 <43190509+twaun95@users.noreply.github.com>
2021-05-06 23:21:02 +0900

    Update 정렬된배열에서특정수의개수구하기.py

--- 이진트리/정렬된배열에서특정수의개수구하기.py
+++ 이진트리/정렬된배열에서특정수의개수구하기.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 n, x = map(int,input().split())
 data = list(map(int,input().split()))
 
-def find_start(data, target, start, end):
+def find_start(data, target, start, end): #시작위치 찾기
     if start > end:
         return -1
 
     mid = (start+end) // 2
-    if data[mid] == target:
+    if data[mid] == target: #찾았을 때 앞에 값 유무 확인
         if data[mid-1] != target:
             return mid
         else:
@@ -19,12 +19,12 @@ def find_start(data, target, start, end):
     else:
         return find_start(data, target, mid+1, end)
 
-def find_end(data, target, start, end):
+def find_end(data, target, start, end): #끝위치 찾기
     if start > end:
         return -1
 
     mid = (start+end) // 2
-    if data[mid] == target:
+    if data[mid] == target: #찾았을 때 뒤에 값 유무 확인
         if data[mid+1] != target:
             return mid
         else:
@@ -36,9 +36,9 @@ def find_end(data, target, start, end):
 
 
 startIdx = find_start(data, x, 0, n-1)
-if startIdx == -1:
+if startIdx == -1: #시작점 못찾았을 때
     print(-1)
 else:
-    endIdx = find_end(data, x, startIdx, n-1)
-    print(endIdx-startIdx+1)
+    endIdx = find_end(data, x, startIdx, n-1) #시작점 찾았으면 끝위치 찾기
+    print(endIdx-startIdx+1)#결과
 
