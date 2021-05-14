094ee1faa94d3e15a0363d2dd2ca107de4aba5dc
twaun95 <43190509+twaun95@users.noreply.github.com>
2021-05-14 23:41:50 +0900

    Update 정수삼각형.py

--- 다이나믹프로그래밍/정수삼각형.py
+++ 다이나믹프로그래밍/정수삼각형.py
@@ -7,16 +7,16 @@ for i in range(n):
 
 for i in range(1,n):
     for j in range(i+1):
-        if j == 0:
+        if j == 0: #해당 위치 기준 위 왼쪽에서 오는 경우가 맨 왼쪽이면 0
             up_left = 0
         else:
             up_left = dp[i-1][j-1]
         
-        if j == i:
+        if j == i: #해당 위치 기준 위 오른쪽에서 오는 경우가 맨 오른쪽이면 0
             up_right = 0
         else:
             up_right = dp[i-1][j]
-
+        # 해당 테이블 값 + 위왼쪽과 위오른쪽에서 오는 값 중 큰 값
         dp[i][j] = dp[i][j] + max(up_right,up_left)
 
 answer = 0
