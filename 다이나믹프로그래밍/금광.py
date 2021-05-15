7ac75680d789504560e6630ee0fdc98cf812827f
twaun95 <43190509+twaun95@users.noreply.github.com>
2021-05-15 23:26:57 +0900

    Update 금광.py

--- 다이나믹프로그래밍/금광.py
+++ 다이나믹프로그래밍/금광.py
@@ -13,19 +13,19 @@ for tt in range(t):
 
     for i in range(1,m):
         for j in range(n):
-            
+            #1.왼위, 범위 벗어나면 -> [0][?]일때 0
             if j == 0:
                 left_up = 0
             else:
                 left_up = dp[j-1][i-1]
-         
+            #2.왼쪽, 범위 벗어나면 0
             left = dp[j][i-1]
-            
+            #3.왼아래, 범위 벗어나면 -> [n-1][?]일때0
             if j == n-1:
                 left_down = 0
             else:
                 left_down = dp[j+1][i-1]
-           
+            #위 3개 최대값에 현재값 더한 값으로 갱신
             dp[j][i] = max(left_up, left_down, left) + dp[j][i]
 
     answer = 0
