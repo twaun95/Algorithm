ee2e786c8384c31a6362d10e80be6c6a82dc7400
twaun95 <>
2021-06-03 15:44:15 +0000

    백준 15483

--- 다이나믹프로그래밍/편집거리.py
+++ 다이나믹프로그래밍/편집거리.py
@@ -1,29 +1,21 @@
-# dp테이블 총 개수만큼 선언
-# 바텀업을 위해 시작부분 초기화
-# 로직찾기
-# 범위 range
-# 비교 (현재값 + dp[현재위치] , dp[비교할 index]) 주의사항 : 현재값 위치에서 전 위치의 값들을 바라봐야함!!
-# 결과 : dp[현재구해야할 값 = index]
-n = int(input())
-dp = []
-for i in range(n):
-    dp.append(list(map(int,input().split())))
+str1 = input()
+str2 = input()
 
-for i in range(1,n):
-    for j in range(i+1):
-        if j==0:
-            up_left = 0
+n = len(str1)
+m = len(str2)
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    dp[i][0] = i
+for j in range(1,m+1):
+    dp[0][j] = j
+
+for i in range(1, n+1): #초기화 해줬기 때문에 1 부터
+    for j in range(1, m+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1]
         else:
-            up_left = dp[i-1][j-1]
-        if j == i:
-            up_right = 0
-        else:
-            up_right = dp[i-1][j]
+            dp[i][j] = min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j]) + 1 #왼,대각, 위
 
-        dp[i][j] = max(up_left, up_right)+dp[i][j]
-
-result = 0
-for i in range(n):
-    result = max(result, dp[n-1][i])
-print(result)
-
+print(dp[i][j])
